4a83b7544881d591b2044f625b2d3a84939eff39
wai-lok <wailokcheng020@gmail.com>
2025-05-12 17:26:06 +0100

    feature: update home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,6 +15,21 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(title: const Text('Home')),
       drawer: MyDrawer(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.public,  // This is the earth/globe icon
+              size: 300,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            const SizedBox(height: 16),  // Add some spacing between icon and text
+            const Text('The third planet in the solar system', 
+              style: TextStyle(fontSize: 24)),
+          ],
+        ),
+      ),
     );
   }
 }
